47a34d829087c2d168abdaa230dff35b99cca0f2
MarcoMuzz <c.musciacchio2@studenti.uniba.it>
2023-01-13 13:42:24 +0100

    Seeder Associazioneutente

--- database/seeders/AssociazioneUtenteSeeder.php
+++ database/seeders/AssociazioneUtenteSeeder.php
@@ -24,5 +24,76 @@ class AssociazioneUtenteSeeder extends Seeder
                 'ruolo' => 3
             ]);
         }
+
+        // ----------- CREAZIONE MEMBRI E MOD 1° ASS -----------
+        for($i=5;$i<=11;$i++) {
+            if ($i == 5 || $i == 6) {
+                Associazione_Utente::create([
+                    'associazione_id' => 1,
+                    'utente_id' => $i,
+                    'ruolo' => 2
+                ]);
+            } else {
+                Associazione_Utente::create([
+                    'associazione_id' => 1,
+                    'utente_id' => $i,
+                    'ruolo' => 1
+                ]);
+
+            }
+        }
+// ----------- CREAZIONE MEMBRI E MOD 2° ASS -----------
+            for($i=12;$i<=18;$i++) {
+                if($i==12 || $i==13){
+                    Associazione_Utente::create([
+                        'associazione_id' => 2,
+                        'utente_id' => $i,
+                        'ruolo' => 2
+                    ]);
+                }
+                else{
+                    Associazione_Utente::create([
+                        'associazione_id' => 2,
+                        'utente_id' => $i,
+                        'ruolo' => 1
+                    ]);
+
+                }
+            }
+        // ----------- CREAZIONE MEMBRI E MOD 3° ASS -----------
+                for($i=19;$i<=25;$i++) {
+                    if ($i == 19 || $i == 20) {
+                        Associazione_Utente::create([
+                            'associazione_id' => 3,
+                            'utente_id' => $i,
+                            'ruolo' => 2
+                        ]);
+                    } else {
+                        Associazione_Utente::create([
+                            'associazione_id' => 3,
+                            'utente_id' => $i,
+                            'ruolo' => 1
+                        ]);
+
+                    }
+                }
+        // ----------- CREAZIONE MEMBRI E MOD 4° ASS -----------
+                    for($i=26;$i<=32;$i++) {
+                        if($i==26 || $i==27){
+                            Associazione_Utente::create([
+                                'associazione_id' => 4,
+                                'utente_id' => $i,
+                                'ruolo' => 2
+                            ]);
+                        }
+                        else{
+                            Associazione_Utente::create([
+                                'associazione_id' => 4,
+                                'utente_id' => $i,
+                                'ruolo' => 1
+                            ]);
+
+                        }
+        }
     }
 }
