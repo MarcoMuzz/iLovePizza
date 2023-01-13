8fdcb7cdc88acab33c8b3738cdd775e7db294d04
GhostNappalm <m.baso@studenti.uniba.it>
2023-01-13 08:58:08 +0100

    Seeder Utente Factory Utente

---
+++ database/seeders/UtenteSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Utente;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UtenteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Utente::factory(10)->create();
+    }
+}
