34d4284145d8a1c5e0dc1711e3c16da3f2dadd04
GhostNappalm <m.baso@studenti.uniba.it>
2023-01-14 16:03:44 +0100

    Esempio Route-Controller-Vista-Model

---
+++ app/Http/Controllers/testController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Associazione;
+use App\Models\Associazione_Utente;
+use App\Models\Utente;
+use Illuminate\Http\Request;
+
+class testController extends Controller
+{
+    private function GetAssociazioneId($id)
+    {
+        if( Associazione_Utente::find($id)!= null)
+            return Associazione_Utente::find($id)->associazione_id;
+        else
+            return null;
+    }
+
+    public function utenteProfilo($id)
+    {
+        $utente=Utente::findOrFail($id);
+        $associazione=$this->GetAssociazioneId($id);
+        if($associazione!= null)
+        {
+            $associazione=Associazione::find($this->GetAssociazioneId($id));
+            return view('utenteProfilo',[
+                'utente' => $utente,
+                'associazione' => $associazione
+            ]);
+        }
+        else
+        {
+            return view('utenteProfilo',[
+                'utente' => $utente,
+                'associazione' => 'null'
+            ]);
+        }
+    }
+}
