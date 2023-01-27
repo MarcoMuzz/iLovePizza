51cc1a0e246f2b4b649c613d80e319847a26ef4c
GhostNappalm <m.baso@studenti.uniba.it>
2023-01-27 12:56:40 +0100

    Modifiche su invitoController, aggiunti i vari casi di email esistenti e di conseguenza i messaggi di errore e successo anche per l'accettazione dell'invito

--- app/Http/Controllers/invitoController.php
+++ app/Http/Controllers/invitoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Mail\mailInvito;
+use App\Models\Associazione;
 use App\Models\Associazione_Utente;
 use App\Models\Invito;
 use App\Models\Utente;
@@ -15,9 +16,14 @@ class invitoController extends Controller
 {
     private function checkEmail($email){
         if(Utente::where('email',$email)->first()==null){
-            return true;
-        }else {
-            return false;}
+            return 0; //email non presente nel db
+        }
+        else{
+            if(Utente::where('email',$email)->first()->Associazione_Utente==null) return 1; //email appartiene ad un untente iscritto
+            if(Utente::where('email',$email)->first()->Associazione_Utente->ruolo>=1) return 2; //email appartiene ad un untente che fa gia parte di un'associazione
+        }
+        return null;
+
 
     }
 
@@ -38,7 +44,7 @@ class invitoController extends Controller
     }
 
     public function inviaInvito(Request $request){
-       if($this->checkEmail($request->email)==true){
+       if($this->checkEmail($request->email)<=1){
             $token=$this->creaToken();
             $invito=new Invito;
             $invito->utente_mail=$request->email;
@@ -51,26 +57,57 @@ class invitoController extends Controller
 
            return back()->with('status','Invito inviato!');
 
-      }else{
-        return back()->with('statusRed','Email non valida');
-     }
-
+       }
+       elseif($this->checkEmail($request->email)==2){
+           return back()->with('statusRed','L\'email appartiene ad un utente che è già membro di un associazione');
+       }
+       elseif($this->checkEmail($request->email)==null){
+           return back()->with('statusRed','Email non valida');
+       }
     }
 
     public function registrazioneInvito($token){
-    if(Auth::check()) Auth::logout();
-            if(Invito::where('token',$token)->first()!=null){
-                $invito= Invito::where('token',$token)->first();
-                return view('registrazioneInvito',[
-                    'invito'=>$invito
-
-                ]);
+        if(Invito::where('token',$token)->first()==null)
+        {
+            return redirect('/')->with('statusRed','L\'invito non è valido');
+        }
+        else
+        {
+            $invitoEmail=Invito::where('token',$token)->first()->utente_mail;
+            $associazione_id=Invito::where('token',$token)->first()->associazione_id;
+            $invito= Invito::where('token',$token)->first();
+            if(Auth::check()){
+                redirect('/');
+                if(Auth::user()->email==$invitoEmail){//ramo registrazione di utente presente nel db e gia loggato
+                    $this->collegamentoUtAss(Auth::user()->id, $associazione_id);
+                    return redirect('/')->with('status','Invito ad associazione accettato!');
+                }
+                else{//ramo registrazione di utente presente nel db e gia loggato ma con un profilo diverso da quello dell'invito
+                    return redirect('/')->with('statusRed','Per accettare l\'invito devi prima fare il logout dall\'attuale profilo');
+                }
             }
-            if(Invito::where('token',$token)->first()==null)
-            {
-                return redirect('/')->with('statusRed','L\'invito non è valido');
+            else{
+                if(Utente::where('email',$invitoEmail)->first()==null)
+                {
+                    return view('registrazioneInvito',[ //ramo registrazione di utente non presente nel db
+                        'invito'=>$invito
+                    ]);
+                }
+                else
+                {
+                    return redirect('/')->with('statusRed','Per accettare l\'invito devi prima fare il login con il profilo invitato :'.$invitoEmail);
+                }
             }
+        }
+    }
 
+    private function collegamentoUtAss($u_id,$a_id){
+        $membro= new Associazione_Utente;
+        $membro->utente_id=$u_id;
+        $membro->associazione_id=$a_id;
+        $membro->ruolo=1;
+        $membro->save();
+        return 0;
     }
 
 
