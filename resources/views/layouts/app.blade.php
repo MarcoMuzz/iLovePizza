73118df614e961f299e975aa893dff0859748f89
GhostNappalm <m.baso@studenti.uniba.it>
2023-01-28 17:52:45 +0100

    Introduzione attributo dusk="user-functions" per chiamate dusk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,10 +78,10 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a  id="navbarDropdown" class="nav-link dropdown-toggle" dusk="user-functions" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->username}}
 
-                                </a>
+                                </a >
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
