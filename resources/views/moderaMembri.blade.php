c29fb31172a8dfa0b152b2c890f44c74a44387a4
MarcoMuzz <c.musciacchio2@studenti.uniba.it>
2023-01-31 12:38:35 +0100

    Modifiche varie

--- resources/views/moderaMembri.blade.php
+++ resources/views/moderaMembri.blade.php
@@ -1,13 +1,89 @@
 @extends('layouts.app')
 @section('title', 'Modera Membri')
 @section('content')
+<style>
+
+    .btn-primary{
+        margin:1px;
+    }
+</style>
+
+    <div class="container">
+        <h1>Modera Membri</h1>
     @if(session('status'))
         <div class="alert alert-success">
             {{ session('status') }}
         </div>
     @endif
+        <table class="table text-center">
+            <thead>
+            <tr>
+                <th scope="col">Username</th>
+                <th scope="col">Ruolo</th>
+                <th scope="col">Azione</th>
+            </tr>
+            </thead>
+            @foreach($membri as $membro)
+                <tr>
+                    <td>{{$membro['username']}}</td>
+                    <td>
+                        @switch($membro['ruolo'])
+                            @case(1)
+                                Membro
+                                @break
+
+                            @case(2)
+                                Moderatore
+                                @break
+
+                            @case(3)
+                                Capo
+                                @break
+
+                        @endswitch
+                    </td>
+                    <td>
+                        <div style="display:inline-flex">
+                        @if($userRole==3 && $membro['ruolo']=='1')
+                            <form action="{{route('promuoviMembro')}}" method="post">
+                                @csrf
+                                <input  type="hidden" name="m_id" value="{{$membro['id']}}$">
+                                <button type="submit" class="btn btn-primary">Promuovi</button>
+                            </form>
+                        @endif
 
 
+                        @if($membro['ruolo']<$userRole && $membro['ruolo']=='2')
+                            <form action="{{route('declassaMembro')}}" method="post">
+                                @csrf
+                                <input  type="hidden" name="m_id" value="{{$membro['id']}}$">
+                                <button type="submit" class="btn btn-primary">Declassa</button>
+                            </form>
+                        @endif
+
+                        @if($membro['ruolo']<$userRole)
+                            <form action="{{route('espelliMembro')}}" method="post">
+                                @csrf
+                                <input  type="hidden" name="m_id" value="{{$membro['id']}}$">
+                                <button type="submit" class="btn btn-primary">Espelli</button>
+                            </form>
+                        @endif
+                        </div>
+                    </td>
+
+                </tr>
+
+            @endforeach
+
+        </table>
+
+
+
+
+
+
+    </div>
+    {{--
         @foreach($membri as $membro)
 
                 {{$membro['username']}}
@@ -54,7 +130,7 @@
                 <hr>
 
         @endforeach
-
+--}}
 
 @endsection
 
